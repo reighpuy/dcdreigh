7547741fa239762efd9c57894559ab947926a2dc
0-0 <39878411+reighpuy@users.noreply.github.com>
2020-05-26 23:49:53 +0700

    Update media.py

--- cogs/media.py
+++ cogs/media.py
@@ -4,9 +4,14 @@ class Media(commands.Cog): # Membuat Cogs
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command() # Membuat Perintah untuk Cogs
-    async def mantap(self, ctx):
-        await ctx.send('hai')
+    @commands.command(brief="Tidur") # Membuat Perintah Kbbi untuk Cogs
+    async def kbbi(self, ctx, arg):
+        if len(arg) > 0:
+          try:
+            data = KBBI(arg)
+            await ctx.send("```Mencari Kata -> {}\n\n{}```".format(arg, data))
+          except:await ctx.send("```Kata '{}' Tidak ditemukan dalam KBBI.```".format(arg))
+        else:await ctx.send("Kata tidak boleh Kosong.")
 
 def setup(bot): # Setup Cogs nya
     bot.add_cog(Media(bot))
